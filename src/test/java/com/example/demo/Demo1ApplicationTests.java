77dcf6523ed338cfa657517fad19040967fa5554
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2022-11-08 22:46:06 +0600

    conditions

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StringUtils;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 class Demo1ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat("Eld").isEqualTo(StringUtils.capitalize(("eld")));
 	}
 
 }
