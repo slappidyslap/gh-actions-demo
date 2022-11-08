fd3faab89e46a7c5383909977ecfd3cce4191b9e
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2022-11-08 22:54:59 +0600

    update

---
+++ src/main/java/com/example/demo/ExampleService.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleService {
+	public String exampleMethod(String str) {
+		return str;
+	}
+}
--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -2,16 +2,20 @@ package com.example.demo;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.StringUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(classes = ExampleService.class)
 class Demo1ApplicationTests {
 
+	@Autowired
+	private ExampleService service;
 	@Test
 	void contextLoads() {
-		assertThat("Eld").isEqualTo(StringUtils.capitalize(("eld")));
+		assertThat("Eld").isEqualTo(service.exampleMethod(("eld")));
 	}
 
 }
